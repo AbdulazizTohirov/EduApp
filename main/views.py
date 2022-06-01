d0460bedb712b8fd6cb1ccab6694b5ea09aea7e5
AbdulazizTohirov <abdulaziztohirov5002@gmail.com>
2022-06-01 17:12:54 +0300

    add index.html

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import redirect,render
+from.models import *
 
-# Create your views here.
+
+def index_view(request):
+    users = User.objects.all()
+    subject_model = SubjectModel.objects.all()
+    context = {
+        'users':users,
+        'subject': subject_model
+    }
+    return render(request, 'index.html', context)
