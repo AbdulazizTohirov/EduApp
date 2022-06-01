bba82a0b9c3204f9da5d5d9fdeb1534cc3031c7c
AbdulazizTohirov <abdulaziztohirov5002@gmail.com>
2022-06-01 22:01:12 +0300

    add api

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import SubjectCreateView
+from .views import DayCreateView, DayDetailView, GroupCreateView, GroupDetailView, SubjectCreateView, SubjectDetailView
 from main.models import SubjectModel
 
 
 urlpatterns = [
-    path('subject/create/', SubjectCreateView.as_view())
+    path('subject/create/', SubjectCreateView.as_view()),
+    path('Subject/detail/<int:pk>/', SubjectDetailView.as_view()),
+    path('day/create/', DayCreateView.as_view()),
+    path('day/detail/<int:pk>/', DayDetailView.as_view()),
+    path('group/create/', GroupCreateView.as_view()),
+    path('group/detail/<int:pk>/', GroupDetailView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -8,3 +8,25 @@ class SubjectCreateView(generics.CreateAPIView):
     queryset = models.SubjectModel.objects.all()
     serializer_class = serializers.SubjectSerializer
 
+
+class SubjectDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.SubjectModel.objects.all()
+    serializer_class = serializers.SubjectSerializer
+
+
+class DayCreateView(generics.CreateAPIView):
+    queryset = models.DayModel.objects.all()
+    serializer_class = serializers.DaySerializer
+
+
+class DayDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.DayModel.objects.all()
+    serializer_class = serializers.DaySerializer
+
+class GroupCreateView(generics.CreateAPIView):
+    queryset = models.GroupModel.objects.all()
+    serializer_class = serializers.GroupSerializer
+    
+class GroupDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.GroupModel.objects.all()
+    serializer_class = serializers.GroupSerializer
